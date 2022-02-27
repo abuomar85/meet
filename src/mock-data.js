2523d0a2b3af04e5a931e9dc3d1fe0d85aefc2f5
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2022-02-27 21:16:12 +0100

    create the test

---
+++ src/mock-data.js
@@ -0,0 +1,37 @@
+const mockData = [
+    {
+        "kind": "calendar#event",
+        "etag": "\"3181159875584000\"",
+        "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200520T120000Z",
+        "status": "confirmed",
+        "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA1MjBUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+        "created": "2020-05-19T19:14:30.000Z",
+        "updated": "2020-05-27T11:45:37.792Z",
+        "summary": "React is Fun",
+        "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
+        "location": "Berlin, Germany",
+        "creator": {
+         "email": "fullstackwebdev@careerfoundry.com",
+         "self": true
+    }}
+    ,
+
+    {
+        "kind": "calendar#event",
+        "etag": "\"3181159875584000\"",
+        "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20200522T120000Z",
+        "status": "confirmed",
+        "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA1MjJUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+        "created": "2020-05-19T19:14:30.000Z",
+        "updated": "2020-05-27T11:45:37.792Z",
+        "summary": "React is Fun",
+        "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
+        "location": "Berlin, Germany",
+        "creator": {
+         "email": "fullstackwebdev@careerfoundry.com",
+         "self": true
+        }
+    }
+];
+  
+  export { mockData };
